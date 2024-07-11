37d54daad725a0c9e0af22b84243ee666cfdb653
Carlos Eduardo Amorim <86995782+eduardoamorim-dev@users.noreply.github.com>
2024-07-11 20:22:19 -0300

    Add files via upload

---
+++ removerSegundoElemento.cpp
@@ -0,0 +1,120 @@
+#include<iostream>
+
+using namespace std;
+
+struct No {
+
+    int valor;
+    No* prox;
+
+    No() {
+        prox = NULL;
+    }
+
+    No(int _valor) {
+        valor = _valor;
+        prox = NULL;
+    }
+
+};
+
+struct Lista {
+
+    No* inicio;
+    No* fim;
+    int n;
+
+    Lista() {
+        inicio = NULL;
+        fim = NULL;
+        n = 0;
+    }
+
+    void inserirInicio(int valor) {
+        No* novo = new No(valor);
+        if (inicio == NULL) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            novo->prox = inicio;
+            inicio = novo;
+        }
+        n++;
+    }
+
+    void inserirFinal(int valor) {
+        No* novo = new No(valor);
+        if (inicio == NULL) {
+            inicio = novo;
+            fim = novo;
+        } else {
+            fim->prox = novo;
+            fim = novo;
+        }
+        n++;
+    }
+
+    void removerInicio() {
+        if (n == 0) return;
+        if (n == 1) {
+            delete(inicio);
+            inicio = NULL;
+            fim = NULL;
+            n--;
+            return;
+        }
+        No* aux = inicio;
+        inicio = inicio->prox;
+        delete(aux);
+        n--;
+    }
+
+    void removerFinal() {
+        if (n == 0) return;
+        if (n == 1) {
+            delete(inicio);
+            inicio = NULL;
+            fim = NULL;
+            n--;
+            return;
+        }
+        No* aux = inicio;
+        while (aux->prox != fim) {
+            aux = aux->prox;
+        }
+        delete(fim);
+        fim = aux;
+        aux->prox = NULL;
+        n--;
+    }
+
+    void imprimir() {
+        No* aux = inicio;
+        //printf("%d %d\n", aux, aux->valor);
+        while (aux != NULL) {
+            printf("%d\n", aux->valor);
+            //printf("%d %d\n", aux->prox, aux->valor);
+            aux = aux->prox;
+        }
+    }
+
+    void removerSegundoElemento() {
+        if (n <= 1) return;
+        No* aux = inicio->prox;
+        inicio->prox = aux->prox;
+        if(aux == fim){
+            fim = inicio;
+        }
+        delete(aux);
+        n--;
+    }
+
+};
+
+int main() {
+
+
+
+
+    return 0;
+}
