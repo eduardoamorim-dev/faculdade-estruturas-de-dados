4183c968532ac43d8e3822fe056716d6478de763
Carlos Eduardo Amorim <86995782+eduardoamorim-dev@users.noreply.github.com>
2024-07-14 19:16:39 -0300

    Create 2-atividade-lista.cpp

---
+++ atividade lista/2-atividade-lista.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std;
+
+struct No {
+    int valor;   
+    No* prox;   
+
+
+    No() {
+        prox = NULL;
+    }
+
+
+    No(int _valor) {
+        valor = _valor;
+        prox = NULL;
+    }
+};
+
+
+struct Lista {
+    No* inicio;   
+    No* fim;    
+    int n;       
+
+
+    Lista() {
+        inicio = NULL;
+        fim = NULL;
+        n = 0;
+    }
+
+
+    void inserirInicio(int valor) {
+        No* novo = new No(valor); 
+        if (inicio == NULL) {
+
+            inicio = novo;
+            fim = novo;
+        } else {
+
+            novo->prox = inicio;
+            inicio = novo;
+        }
+        n++; 
+    }
+
+
+    void inserirFinal(int valor) {
+        No* novo = new No(valor); 
+        if (inicio == NULL) {
+
+            inicio = novo;
+            fim = novo;
+        } else {
+
+            fim->prox = novo;
+            fim = novo;
+        }
+        n++; 
+    }
+
+
+    void imprimir() {
+        No* aux = inicio; 
+        while (aux != NULL) {
+            printf("%d\n", aux->valor); 
+            aux = aux->prox;            
+        }
+    }
+    
+    void removerSegundo() {
+        if (n <= 1) return; 
+
+        No* segundo = inicio->prox;
+        inicio->prox = segundo->prox; 
+
+        if (segundo == fim) { 
+            fim = inicio; 
+        }
+
+        delete segundo; 
+        n--; 
+    }
+};
+
+
+int main() {
+    
+    Lista l; 
+
+
+    l.inserirInicio(10); 
+    l.inserirInicio(5);  
+    
+    l.inserirFinal(7);   
+    l.inserirFinal(3);   
+
+    l.imprimir(); 
+
+    cout << "Removendo o segundo elemento:" << endl;
+    l.removerSegundo();
+    l.imprimir();
+
+    return 0; 
+}
